db81c5cb22edc77b8695e3e21fbaff7ad38b3bb2
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-17 15:43:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
   end 
   
   def kind
-    if 
+    if @a <= 0 || @b <= 0 || @c <= 0 || @a + @b < @c || @b + @c < @a || @a + @c < @b 
     
     elsif @a == @b && @b == @c 
       :equilateral
